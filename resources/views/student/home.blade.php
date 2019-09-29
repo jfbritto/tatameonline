09a3f100bfa0a1fb1eb919565b6782f055455215
João Filipi <jf.britto@hotmail.com>
2019-09-29 19:14:10 -0300

    Melhorias

--- resources/views/student/home.blade.php
+++ resources/views/student/home.blade.php
@@ -22,7 +22,7 @@
 
 <div class="box">
     <div class="box-header">
-        <h3 class="box-title"><i class="fas fa-user-check"></i>&nbsp;&nbsp;últimas presenças</h3>
+        <h3 class="box-title"><i class="fas fa-user-check"></i>&nbsp;&nbsp;Últimas presenças</h3>
     </div>
     <!-- /.box-header -->
     <div class="box-body table-responsive no-padding">
