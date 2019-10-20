33361ae1f5d2a89aebd0391e2cc7db78c5b46215
João Filipi <jf.britto@hotmail.com>
2019-10-19 23:55:02 -0300

    Melhorias

--- public/js/Admin/student/homeStudent.js
+++ public/js/Admin/student/homeStudent.js
@@ -141,7 +141,7 @@ function list(id)
                             <td class="hidden-xs">${data.data[i].email}</td>
                             <td id="status${data.data[i].id}" class="hidden-xs">${data.data[i].isActive==1?'<span class="label label-success">Ativo</span>':'<span class="label label-danger">Inativo</span>'}</td>
                             <td class="hidden-xs">${data.data[i].aulas}</td>
-                            <td>
+                            <td style="width:130px">
                                 <div class="input-group-btn">
                                     <a class="btn btn-primary btn-sm pull-right" href="/admin/student/show/${data.data[i].id}" title="Abrir aluno"><i class="fas fa-sign-in-alt"></i></a>
 
