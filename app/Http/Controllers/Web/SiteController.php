0e7237b2411d6a0552c39baa64e3849886f0c288
João Filipi <jf.britto@hotmail.com>
2019-10-21 16:29:15 -0300

    m

--- app/Http/Controllers/Web/SiteController.php
+++ app/Http/Controllers/Web/SiteController.php
@@ -11,12 +11,12 @@ class SiteController extends Controller
     public function index()
     {
 
-        $isMobile = preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
-        // $isApp = preg_match("/(GoNativeAndroid)/i", $_SERVER["HTTP_USER_AGENT"]);
+        // $isMobile = preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+        $isApp = preg_match("/(Build)/i", $_SERVER["HTTP_USER_AGENT"]);
 
         $academies = Academy::where('isActive', '=', 1)->get();
 
-        if($isMobile){
+        if($isApp){
             echo $_SERVER["HTTP_USER_AGENT"];
             // return view('index', ['academies'=>$academies]);
         }else{
